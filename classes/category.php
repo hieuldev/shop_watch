bcf071dc3fdd5ff9c6b9ca75306ec3264e28ceda
hieun <hieunguyenquoc1999@gmail.com>
2021-12-11 21:11:36 +0700

    update cat and customer

--- classes/category.php
+++ classes/category.php
@@ -1,8 +1,8 @@
 <?php
 
-	$filepath = realpath(dirname(__FILE__));
-	include_once ($filepath.'/../lib/database.php');
-	include_once ($filepath.'/../helpers/format.php');
+$filepath = realpath(dirname(__FILE__));
+include_once($filepath . '/../lib/database.php');
+include_once($filepath . '/../helpers/format.php');
 ?>
 
 <?php
@@ -12,16 +12,19 @@
 class category
 {
 	private $db;
+	private $db2;
 	private $fm;
 	public function __construct()
 	{
-		$this->db = new Database();
+		$database = new Database();
+		$this->db = $database->data->tbl_category;
+		$this->db2 = $database->data->tbl_product;
 		$this->fm = new Format();
 	}
 	public function insert_category($catName)
 	{
 		$catName = $this->fm->validation($catName); //gọi ham validation từ file Format để ktra
-		$catName = mysqli_real_escape_string($this->db->link, $catName);
+		
 		//mysqli gọi 2 biến. (catName and link) biến link -> gọi conect db từ file db
 
 		if (empty($catName)) {
@@ -29,14 +32,12 @@ class category
 			return $alert;
 		} else {
 
-			$query = "select *from tbl_category where catName='$catName'";
-			$result = $this->db->select($query);
+			$result = $this->db->findOne(['catName'=>$catName]);
 			if ($result != false) {
 				$alert = "<span class='error'>Danh mục sản phẩm đã tồn tại";
 				return $alert;
 			} else {
-				$query = "INSERT INTO tbl_category(catName) VALUES('$catName') ";
-				$result = $this->db->insert($query);
+				$result = $this->db->insertOne(['catName'=>$catName]);
 				if ($result) {
 					$alert = "<span class='success'>Thêm danh mục sản phẩm thành công</span>";
 					return $alert;
@@ -49,24 +50,18 @@ class category
 	}
 	public function show_category()
 	{
-		$query = "SELECT * FROM tbl_category order by catId desc ";
-		$result = $this->db->select($query);
-
+		$result = $this->db->find();
 		return $result;
-
-		
 	}
 	public function update_category($catName, $id)
 	{
 		$catName = $this->fm->validation($catName); //gọi ham validation từ file Format để ktra
-		$catName = mysqli_real_escape_string($this->db->link, $catName);
-		$id = mysqli_real_escape_string($this->db->link, $id);
+		$id = $this->fm->validation($id);
 		if (empty($catName)) {
 			$alert = "<span class='error'>Danh mục không được để trống</span>";
 			return $alert;
 		} else {
-			$query = "UPDATE tbl_category SET catName= '$catName' WHERE catId = '$id' ";
-			$result = $this->db->update($query);
+			$result = $this->db->updateOne(['_id'=>new MongoDB\BSON\ObjectId($id)],['$set'=>['catName'=>$catName]]);
 			if ($result) {
 				$alert = "<span class='success'>Cập nhật danh mục thành công</span>";
 				return $alert;
@@ -78,8 +73,7 @@ class category
 	}
 	public function del_category($id)
 	{
-		$query = "DELETE FROM tbl_category where catId = '$id' ";
-		$result = $this->db->delete($query);
+		$result = $this->db->deleteOne(['_id'=>new MongoDB\BSON\ObjectId($id)]);
 		if ($result) {
 			$alert = "<span class='success'>Xóa danh mục thành công</span>";
 			return $alert;
@@ -90,36 +84,32 @@ class category
 	}
 	public function getcatbyId($id)
 	{
-		$query = "SELECT * FROM tbl_category where catId = '$id' ";
-		$result = $this->db->select($query);
+		$result = $this->db->findOne(['_id'=>new MongoDB\BSON\ObjectId($id)]);
 		return $result;
 	}
 	public function show_category_fontend()
 	{
-		$query = "SELECT * FROM tbl_category order by catId desc ";
-		$result = $this->db->select($query);
+		//$query = "SELECT * FROM tbl_category order by catId desc ";
+		$result = $this->db->find();
 		return $result;
 	}
 	public function get_product_by_cat($id)
 	{
-		$sp_tungtrang=8;
-			if(!isset($_GET['page']))
-		   {
-			   $page=1;
-		   }
-		   else
-		   {
-			   $page=$_GET['page'];
-		   }
-		   $tung_trang=($page-1)*$sp_tungtrang;
-		$query = "SELECT * FROM tbl_product where catId = '$id' order by catId desc LIMIT $tung_trang,$sp_tungtrang";
-		$result = $this->db->select($query);
+		$sp_tungtrang = 8;
+		if (!isset($_GET['page'])) {
+			$page = 1;
+		} else {
+			$page = $_GET['page'];
+		}
+		$tung_trang = ($page - 1) * $sp_tungtrang;
+		//$query = "SELECT * FROM tbl_product where catId = '$id' order by catId desc LIMIT $tung_trang,$sp_tungtrang";
+		$result = $this->db2->find(['catId'=>$id]);
 		return $result;
 	}
 	public function select_product_by_cat($id)
 	{
-		$query = "SELECT COUNT(*) FROM tbl_product where catId = '$id'";
-		$result = $this->db->select($query);
+		//$query = "SELECT COUNT(*) FROM tbl_product where catId = '$id'";
+		$result = $this->db2->count(['catId'=>$id]);
 		return $result;
 	}
 	public function get_name_by_cat($id)
--- classes/customer.php
+++ classes/customer.php
@@ -13,35 +13,37 @@ include_once($filepath . '/../helpers/format.php');
 class customer
 {
 	private $db;
+	private $db2;
 	private $fm;
 	public function __construct()
 	{
-		$this->db = new Database();
+		$database=new Database();
+        $this->db=$database->data->tbl_customer;
+		$this->db2=$database->data->tbl_news;
 		$this->fm = new Format();
 	}
 	public function insert_customer($data)
 	{
-		$name = mysqli_real_escape_string($this->db->link, $data['name']);
-		$city = mysqli_real_escape_string($this->db->link, $data['city']);
-		$zipcode = mysqli_real_escape_string($this->db->link, $data['zipcode']);
-		$email = mysqli_real_escape_string($this->db->link, $data['email']);
-		$address = mysqli_real_escape_string($this->db->link, $data['address']);
-		$country = mysqli_real_escape_string($this->db->link, $data['country']);
-		$phone = mysqli_real_escape_string($this->db->link, $data['phone']);
-		$password = mysqli_real_escape_string($this->db->link, md5($data['password']));
+		$name =  $this->fm->validation($data['name']);
+		$city = $this->fm->validation($data['city']);
+		$zipcode = $this->fm->validation($data['zipcode']);
+		$email = $this->fm->validation($data['email']);
+		$address = $this->fm->validation($data['address']);
+		$country = $this->fm->validation($data['country']);
+		$phone = $this->fm->validation($data['phone']);
+		$password = $this->fm->validation($data['password']);
 
 		if ($name == "" || $city == "" || $zipcode == "" || $email == "" || $address == "" || $country == "" || $phone == "" || $password == "") {
 			$alert = "<span class='error'>Không được để trống</span>";
 			return $alert;
 		} else {
-			$check_email = "SELECT * FROM tbl_customer WHERE email='$email' LIMIT 1";
+			$check_email =$this->db->findOne(['email'=>$email]);
 			$result_check = $this->db->select($check_email);
 			if ($result_check) {
 				$alert = "<span class='error'>Địa chỉ Email đã tồn tại ? Hãy điền Email khác </span>";
 				return $alert;
 			} else {
-				$query = "INSERT INTO tbl_customer(name,address,city,country,zipcode,phone,email,password) VALUES('$name','$address','$city','$country','$zipcode','$phone','$email','$password') ";
-				$result = $this->db->insert($query);
+				$result = $this->db->insertOne(['name'=>$name,'city'=>$city,'zipcode'=>$zipcode,'email'=>$email,'address'=>$address,'country'=>$country,'phone'=>$phone,'password'=>$password]);
 				if ($result) {
 					$alert = "<span class='success'>Thêm khách hàng thành công</span>";
 					return $alert;
@@ -60,8 +62,8 @@ class customer
 			$alert = "<span class='error'>Email và Pasword không được để trống</span>";
 			return $alert;
 		} else {
-			$check_login = "SELECT * FROM tbl_customer WHERE email='$email' AND password='$password' ";
-			$result_check = $this->db->select($check_login);
+			//$check_login = "SELECT * FROM tbl_customer WHERE email='$email' AND password='$password' ";
+			$result_check = $this->db->findOne(['email'=>$email,'password'=>$password]);
 			if ($result_check) {
 				$value = $result_check->fetch_assoc();
 				Session::set('customer_login', true);
@@ -76,14 +78,14 @@ class customer
 	}
 	public function show_customers($id)
 	{
-		$query = "SELECT * FROM tbl_customer,tbl_customerrank WHERE tbl_customer.RankCID=tbl_customerrank.RankCID and customer_id='$id'";
-		$result = $this->db->select($query);
+		//$query = "SELECT * FROM tbl_customer,tbl_customerrank WHERE tbl_customer.RankCID=tbl_customerrank.RankCID and customer_id='$id'";
+		$result = $this->db->findOne(['_id'=>new MongoDB\BSON\ObjectId($id)]);
 		return $result;
 	}
 	public function show_all_customers()
 	{
-		$query = "SELECT * FROM tbl_customer";
-		$result = $this->db->select($query);
+		//$query = "SELECT * FROM tbl_customer";
+		$result = $this->db->find();
 		return $result;
 	}
 	public function show_user_profile()
@@ -105,24 +107,24 @@ class customer
 	
 	public function show_news($type)
 	{
-		$query = "SELECT * FROM `tbl_news` WHERE newsType='$type'";
-		$result = $this->db->select($query);
+		//$query = "SELECT * FROM `tbl_news` WHERE newsType='$type'";
+		$result = $this->db2->fine(['newsType'=>$type]);
 		return $result;
 	}
 	public function update_customers($data, $id)
 	{
-		$name = mysqli_real_escape_string($this->db->link, $data['name']);
-		$zipcode = mysqli_real_escape_string($this->db->link, $data['zipcode']);
-		$email = mysqli_real_escape_string($this->db->link, $data['email']);
-		$address = mysqli_real_escape_string($this->db->link, $data['address']);
-		$phone = mysqli_real_escape_string($this->db->link, $data['phone']);
+		$name =  $this->fm->validation($data['name']);
+		$zipcode = $this->fm->validation($data['zipcode']);
+		$email = $this->fm->validation($data['email']);
+		$address = $this->fm->validation($data['address']);
+		$phone = $this->fm->validation($data['phone']);
 
 		if ($name == "" || $zipcode == "" || $email == "" || $address == "" || $phone == "") {
 			$alert = "<span class='error'>Không được để trống</span>";
 			return $alert;
 		} else {
-			$query = "update tbl_customer SET name='$name',zipcode='$zipcode',email='$email',address='$address',phone='$phone' WHERE customer_id ='$id'";
-			$result = $this->db->insert($query);
+			//$query = "update tbl_customer SET name='$name',zipcode='$zipcode',email='$email',address='$address',phone='$phone' WHERE customer_id ='$id'";
+			$result = $this->db->updateOne(['_id'=>new MongoDB\BSON\ObjectId($id)],['$set'=>['name'=>$name,'zipcode'=>$zipcode,'email'=>$email,'address'=>$address,'phone'=>$phone]]);
 			if ($result) {
 				$alert = "<span class='success'>Khách hàng được cập nhật thành công</span>";
 				return $alert;
